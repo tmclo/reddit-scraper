826764c49fa5032bd8926b0b39c44e0cd736a7cd
Tom McLoughlin <thomas@springsy.co.uk>
2022-04-15 22:38:07 +0100

    finished :)

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 'use strict';
 const snoowrap = require('snoowrap');
 const snoostorm = require('snoostorm');
+const download = require('image-downloader')
 
 const creds = require("./credentials.json");
 
 const client = new snoowrap(creds);
 
   const submissions = new snoostorm.SubmissionStream(client, {
-    subreddit: "AskReddit",
+    subreddit: "AskReddit", //put the subreddit here! :)
     limit: 10,
     pollTime: 1000,
   });
   submissions.on("item", (item) => {
-      console.log("New post: " + item.title)
+      check(item)
   });
+
+async function check(it) {
+    if(it.post_hint == "image") {
+        dl(it)
+    } else {
+        console.log("item not an image, skipping...")
+    }
+}
+
+async function dl(it) {
+    const options = {
+        url: it.url,
+        dest: './images'
+      }
+    
+      download.image(options)
+      .then(({ filename }) => {
+        console.log('Saved to', filename) 
+      })
+      .catch((err) => console.error(err))
+}
